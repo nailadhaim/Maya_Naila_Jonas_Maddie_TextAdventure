6d0fbd0f02a674c8c557be6f3dc7deccff2a0203
Maya <maya.sliman@student.ap.be>
2025-10-22 19:46:57 +0200

    Item and Inventory klassen aangemaakt

---
+++ Maya_Naila_Jonas_Maddie_TextAdventure/Inventory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maya_Naila_Jonas_Maddie_TextAdventure
+{
+    public class Inventory
+    {
+        private List<Item> items = new();
+
+        public bool Add(Item item)
+        {
+            items.Add(item);
+            return true;
+        }
+
+        public bool Remove(string itemId)
+        {
+            var item = items.FirstOrDefault(i => i.Id == itemId);
+            if (item == null) return false;
+            items.Remove(item);
+            return true;
+        }
+
+        public bool Has(string itemId)
+        {
+            return items.Any(i => i.Id == itemId);
+        }
+
+        public IEnumerable<Item> All()
+        {
+            return items.AsReadOnly();
+        }
+    }
+}
+
---
+++ Maya_Naila_Jonas_Maddie_TextAdventure/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maya_Naila_Jonas_Maddie_TextAdventure
+{
+    public class Item
+    {
+        public string Id { get; }
+        public string Name { get; }
+        public string Description { get; }
+
+        public Item(string id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Id}): {Description}";
+        }
+    }
+}
