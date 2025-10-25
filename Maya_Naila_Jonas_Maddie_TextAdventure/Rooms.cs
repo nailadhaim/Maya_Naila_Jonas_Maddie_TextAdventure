9794cb6c3cd99cbbe3689ecb5d15092be8119f10
Naila Dhaim <naila.dhaim@student.ap.be>
2025-10-25 14:16:52 +0200

    huidige kamer en alle kamers toegevoegd

--- Maya_Naila_Jonas_Maddie_TextAdventure/Rooms.cs
+++ Maya_Naila_Jonas_Maddie_TextAdventure/Rooms.cs
@@ -8,5 +8,14 @@ namespace Maya_Naila_Jonas_Maddie_TextAdventure
 {
     internal class Rooms
     {
+        public Room CurrentRoom { get; private set; }
+
+        private Room start;
+        private Room left;
+        private Room right;
+        private Room up;
+        private Room down;
+        private Room deeper;
+
     }
 }
