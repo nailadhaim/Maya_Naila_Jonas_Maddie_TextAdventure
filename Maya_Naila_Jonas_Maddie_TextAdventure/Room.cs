167261047541e551aaa1698b449acf419deeb173
Naila Dhaim <naila.dhaim@student.ap.be>
2025-10-25 13:44:52 +0200

    Room klasse aangemaakt

---
+++ Maya_Naila_Jonas_Maddie_TextAdventure/Room.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maya_Naila_Jonas_Maddie_TextAdventure
+{
+    internal class Room
+    {
+    }
+}
