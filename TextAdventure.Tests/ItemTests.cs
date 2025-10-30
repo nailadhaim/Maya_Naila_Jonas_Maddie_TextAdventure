884777779b7349fa1b0d3625b057f8b9345b6ba3
Maya <maya.sliman@student.ap.be>
2025-10-30 12:45:58 +0100

    testing-aanpassingen

---
+++ TextAdventure.Tests/ItemTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Maya_Naila_Jonas_Maddie_TextAdventure;
+
+namespace TextAdventure.Tests
+{
+    [TestClass]
+    public sealed class ItemTests
+    {
+        [TestMethod]
+        public void Constructor_AssignsPropertiesCorrectly()
+        {
+            Item sword = new Item("sword", "Sword", "A sharp weapon");
+            Assert.AreEqual("sword", sword.Id);
+            Assert.AreEqual("Sword", sword.Name);
+            Assert.AreEqual("A sharp weapon", sword.Description);
+        }
+
+        [TestMethod]
+        public void ToString_ReturnsCorrectFormat()
+        {
+            Item sword = new Item("sword", "Sword", "A sharp weapon");
+            Assert.AreEqual("Sword (sword): A sharp weapon", sword.ToString());
+        }
+    }
+}
