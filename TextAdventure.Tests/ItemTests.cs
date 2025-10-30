86260839e7cccacaaf5683559e950ea14b7d3eaf
Maya <maya.sliman@student.ap.be>
2025-10-30 13:16:56 +0100

    aanpassingen

---
+++ TextAdventure.Tests/ItemTests.cs
@@ -0,0 +1,11 @@
+﻿namespace TextAdventure.Tests
+{
+    [TestClass]
+    public sealed class ItemTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
